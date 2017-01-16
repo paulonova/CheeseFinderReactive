c903070e4e4348b68dd324cafa93d217d5cab707
paulo <paulonova144@gmail.com>
2017-01-16 14:58:54 +0100

    first commit.

---
+++ app/src/main/java/com/raywenderlich/cheesefinder/CheeseActivity.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.cheesefinder;
+
+
+import android.view.View;
+
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Cancellable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+public class CheeseActivity extends BaseSearchActivity {
+
+    protected void onStart(){
+        super.onStart();
+        Observable<String> searchTextObservable = createButtonClickObservable();
+
+        searchTextObservable
+                //First, specify that the next operator should be called on the I/O thread.
+                .observeOn(Schedulers.io())
+                // For each search query, you return a list of results.
+                .map(new Function<String, List<String>>() {
+                    @Override
+                    public List<String> apply(String result) throws Exception {
+                        return mCheeseSearchEngine.search(result);
+                    }
+                })
+                // Finally, specify that code down the chain should be executed on the main thread instead of on the I/O thread.
+                // In Android, all code that works with Views should execute on the main thread.
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<List<String>>() {
+                    @Override
+                    public void accept(List<String> results) throws Exception {
+                        showResult(results);
+                    }
+                });
+
+
+    }
+
+
+
+    //method that returns an observable that will emit strings.
+    private Observable<String> createButtonClickObservable(){
+
+
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) throws Exception {
+
+                mSearchButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        emitter.onNext(mQueryEditText.getText().toString());
+                    }
+                });
+
+                emitter.setCancellable(new Cancellable() {
+                    @Override
+                    public void cancel() throws Exception {
+                        mSearchButton.setOnClickListener(null);
+                    }
+                });
+            }
+        });
+    }
+
+}
